e5f172e1b30f4b1f466325250f63618e3361eb15
Jowhar <muhammadjouhuk57@gmail.com>
2021-11-25 14:44:11 +0300

    'message'

---
+++ src/StoryReel.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Story from './Story';
+import './StoryReel.css';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+         <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         title="name1" />
+          <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         title="name2" />
+          <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         title="name2" />
+         <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         title="name2" />
+         <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaKRN39DCZDNfcA92qZeA_TRaZwqmIPXn9w&usqp=CAU"
+         title="name2" />
+            
+        </div>
+    )
+}
+
+export default StoryReel
